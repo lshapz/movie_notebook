1bc1d5d901ef57d11f07c7248715fb6fea1f4142
lshapz <laurashapiro01@gmail.com>
2016-10-24 12:42:50 -0400

    linking

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@ class Movie < ApplicationRecord
   belongs_to :director
   validates :title, presence: true
   validates :year, presence: true, numericality: {greater_than: 1880, message: "is before movies existed"}
-  #validates :rating, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rating, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
