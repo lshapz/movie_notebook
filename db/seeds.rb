c094c5cd3e4dd1f1eb00588312e73e33f833ce8c
lshapz <laurashapiro01@gmail.com>
2016-10-24 12:50:08 -0400

    nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Director.create(name: "Gus")
-Director.create(name: "Matt")
-Movie.create([{title: "Star Wars", director_id: 1}, {title: "Episode 8", director_id: 2}, {title: "Episode never", director_id: 1}])
+Director.create(name: "Alan Smithee")
+#Movie.create([{title: "Star Wars", director_id: 1}, {title: "Episode 8", director_id: 2}, {title: "Episode never", director_id: 1}])
