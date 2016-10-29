68f3061070967e5d7bed0114ce8ebf4646150a2b
lshapz <laurashapiro01@gmail.com>
2016-10-29 14:44:50 -0400

    basic bootstrap css

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -15,17 +15,22 @@ def edit
 end
 
 def update
-  @user_movie = UserMovie.find_or_create_by(movie_id: params[:movie], user_id: current_user)
-  
   # byebug
-  if @user_movie.update(rating: params[:rating], big_screen: params[:big_screen], year_seen: params[:year_seen])
-    redirect_to movie_path(@user_movie.movie)
+  if UserMovie.find_by(movie_id: params[:movie], user_id: current_user)
+    @user_movie = UserMovie.find_by(movie_id: params[:movie], user_id: current_user)
+  # byebug
   else
-    # byebug
-    @opinion = @user_movie 
-    @movie = Movie.find(params[:movie])
-    render :edit
-  end
+    @user_movie = @user_movie = UserMovie.new(movie_id: params[:movie], user_id: current_user)
+  end 
+    if @user_movie.update(rating: params[:rating], big_screen: params[:big_screen], year_seen: params[:year_seen])
+      redirect_to movie_path(@user_movie.movie)
+    else
+      # byebug
+      @opinion = @user_movie 
+      @movie = Movie.find(params[:movie])
+      render :edit
+    end
+  else 
 end
 
 
